d9ca7e19e083f1e5f91405cbc634fda33acde9f2
Virgilio Muñoz <vamr169@hotmail.com>
2022-07-21 15:36:28 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,46 @@
+
+# Django register template library
+from django import template
+register = template.Library()
+
+# Django user model check
+from django.contrib.auth import get_user_model
+user_model = get_user_model()
+
+# Django escape the dangerous user supplied data
+# from django.utils.html import escape 
+# from django.utils.safestring import mark_safe
+
+# Django escapes html values and marks strings safe 
+# in a single step
+from django.utils.html import format_html
+
+
+@register.filter
+def author_details(author, current_user=None):
+  """ Show author first and last name if available  """
+  
+  if not isinstance(author, user_model):
+    # return empty string as safe default
+    return ""
+  
+  if author == current_user:
+        return format_html("<strong>me</strong>")
+
+  if author.first_name and author.last_name:
+    name = f"{author.first_name} {author.last_name}"
+  else:
+    name = f"{author.username}"
+
+  if author.email:
+    # email = author.email
+    # prefix = f'<a href="mailto:{email}">'
+    # suffix = "</a>"
+    prefix = format_html('<a href="mailto:{}">', author.email)
+    suffix = format_html("</a>")
+  else:
+    prefix = ""
+    suffix = ""
+
+  # return mark_safe(f"{prefix}{name}{suffix}")
+  return format_html('{}{}{}', prefix, name, suffix)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
+
+# Django
 from django.shortcuts import render
+from django.utils import timezone
+
+# Models
+from blog.models import Post
+
 
 def index(request):
-  return render(request, 'blog/index.html', {})
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
