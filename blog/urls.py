de85551024c1e3054fa4cd8d7d3ad0734aa538f7
Virgilio Muñoz <vamr169@hotmail.com>
2022-08-10 20:34:26 +0000

    Finish First-Party Django REST API

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 """blog URL Configuration"""
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='blog'),
     path('post/<slug>/', views.post_detail, name='blog-post-detail'),
+    path('api/v1/', include('blog.api_urls')),
 ]
