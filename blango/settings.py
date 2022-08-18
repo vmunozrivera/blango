d744d2b163988f27c0d6f40f1dfad33ffa2de42d
Virgilio Muñoz <vamr169@hotmail.com>
2022-08-18 00:41:18 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework',
         # In House
         'blog',
         'blango_auth',
